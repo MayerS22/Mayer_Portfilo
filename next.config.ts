41f19b7a33130338c1fc9fa088fbaecbf399a4b3
MayerS22 <mayersoliman4@gmail.com>
2025-08-20 23:16:56 +0300

    fixing config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
